5072fc8222b02a81db6886f6b35f490b23377007
Eddie <ebracho@mail.csuchico.edu>
2016-01-28 00:20:57 -0800

    Implemented floyd_asps and floyd_path

--- src/EveTS.py
+++ src/EveTS.py
@@ -28,12 +28,47 @@ def build_system_adjacency_matrix(region):
     normalized_system_ids = { s:i for i,s in enumerate(systems) }
     n_systems = len(systems)
 
-    adj_mat = np.full((n_systems, n_systems), None)
+    adj_mat = np.full((n_systems, n_systems), np.nan)
     for s1, s2 in get_adjacent_system_pairs(region):
         s1_n = normalized_system_ids[s1]
         s2_n = normalized_system_ids[s2]
         adj_mat[s1_n][s2_n] = 1
         
     return adj_mat
+
+def floyd_apsp(adj_mat):
+    n = len(adj_mat[0])
+    dist_mat = np.full(adj_mat.shape, np.inf)
+    next_mat = np.full(adj_mat.shape, np.nan)
+
+    # initalize dist_mat, next_mat
+    for i in range(n):
+        for j in range(n): 
+            if not np.isnan(adj_mat[i][j]):
+                dist_mat[i][j] = adj_mat[i][j]
+                next_mat[i][j] = j
+
+    # Find shortest path between all pairs
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist_mat[i][k] + dist_mat[k][j] < dist_mat[i][j]:
+                    dist_mat[i][j] = dist_mat[i][k] + dist_mat[k][j]
+                    next_mat[i][j] = next_mat[i][k]
+
+    return dist_mat.astype(np.int), next_mat.astype(np.int)
+
+def floyd_path(next_mat, u, v): 
+    if np.isnan(next_mat[u][v]):
+        return []
+    path = [u]
+    while u != v:
+        u = next_mat[u][v]
+        path.append(u)
+    return path
+
+adj_mat = build_system_adjacency_matrix('The Bleak Lands')
+dist_mat, next_mat = floyd_apsp(adj_mat)
+print(floyd_path(next_mat, 1, 4))
 
 conn.close()
